03a7e241021063aa9f5849e07a621c40fbb02f54
Assem Amr <62268263+assemAA@users.noreply.github.com>
2023-02-12 14:36:16 +0200

    Update productsModel.js

--- models/productsModel.js
+++ models/productsModel.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose')
 const autoIncrement = require('mongoose-sequence')(mongoose)
 
 const productSchema = new mongoose.Schema({
-    _id : {type : mongoose.Schema.Types.ObjectId} , 
-    prodID : {type : Number} ,
+    _id : {type : Number} , 
     productName : {type : String , required : true } ,
     description : {type : String , required : true} , 
     price : {type : Number , required : true} , 
     productImage : {type : String , required : true} 
-})
+} , {_id : false})
 
-productSchema.plugin(autoIncrement , {inc_field: 'prodID'})
+productSchema.plugin(autoIncrement , {inc_field: '_id'})
 
 mongoose.model("Products" ,  productSchema)
